2d5f5f2e2ad843fa138dd44b4dac7fa8664fc031
Gordon <me@gordonkirsch.dev>
2024-02-23 18:40:03 +0100

    Initial Commit

---
+++ GroupCam/GroupCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupCamApp.swift
+//  GroupCam
+//
+//  Created by Gordon on 23.02.24.
+//
+
+import SwiftUI
+
+@main
+struct GroupCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

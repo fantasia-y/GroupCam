d0f1764deb217e3922a3f6a9f6e035fb50a9e8d1
Gordon <me@gordonkirsch.dev>
2024-02-25 21:24:31 +0100

    ci pre build script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -4,7 +4,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE_PATH/ci_scripts || exit 1
+cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"BASE_URL\":\"%s\",\"PUSHER_INSTANCE_ID\":\"%s\",\"AWS_ACCESS_KEY_ID\":\"%s\",\"AWS_SECRET_ACCESS_KEY\":\"%s\"}" "$BASE_URL" "$PUSHER_INSTANCE_ID" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" >> ../GroupCam/SupportingFiles/secrets.json

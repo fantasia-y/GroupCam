1ff86c2b97c9450be17e1f7610ea9f61bdacdafe
Gordon <me@gordonkirsch.dev>
2024-02-25 22:10:55 +0100

    ci post clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,12 +4,14 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_PRIMARY_REPOSITORY_PATH || exit 1
+cd $CI_PRIMARY_REPOSITORY_PATH/GroupCam || exit 1
+
+echo $(ls)
 
 # Write a JSON File containing all the environment variables and secrets.
-touch ./GroupCam/SupportingFiles/secrets.json
+touch SupportingFiles/secrets.json
 
-printf "{\"BASE_URL\":\"%s\",\"PUSHER_INSTANCE_ID\":\"%s\",\"AWS_ACCESS_KEY_ID\":\"%s\",\"AWS_SECRET_ACCESS_KEY\":\"%s\"}" "$BASE_URL" "$PUSHER_INSTANCE_ID" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" >> ./GroupCam/SupportingFiles/secrets.json
+printf "{\"BASE_URL\":\"%s\",\"PUSHER_INSTANCE_ID\":\"%s\",\"AWS_ACCESS_KEY_ID\":\"%s\",\"AWS_SECRET_ACCESS_KEY\":\"%s\"}" "$BASE_URL" "$PUSHER_INSTANCE_ID" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" >> SupportingFiles/secrets.json
 
 echo "Wrote Secrets.json file."
 
